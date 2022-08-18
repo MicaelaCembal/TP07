4bf6415095b6edba7e98f604c4fd1f573dd1da13
AGUSTINA YAEL ANGEL <46645633@ort>
2022-08-18 14:48:49 -0300

    archivos

--- TP07/Models/Juego.cs
+++ TP07/Models/Juego.cs
@@ -25,3 +25,81 @@ correcta o incorrecta. Previo a devolver el booleano realiza dos acciones:
 puntos a _puntajeActual (la definen ustedes) y suma 1 respuesta correcta
 en _cantidadPreguntasCorrectas.
 2. Elimina la pregunta enviada por parámetro de la lista de preguntas*/
+
+using System.Collections.Generic;
+namespace TP07.Models{
+
+public class Pregunta
+{
+    /*CREAR LISTAS*/
+
+    private string _username;
+    private int _puntajeActual;
+    private int _cantidadPreguntasCorrectas;
+    private List<Pregunta> _preguntas;
+    private List<Respuesta> _respuestas;
+   
+    public Preguntas(string username,int puntajeActual,int cantidadPreguntasCorrectas,List<Pregunta> preguntas, List<Respuesta> respuestas)
+        {
+           _username= username;
+           _puntajeActual=puntajeActual;
+           _cantidadPreguntasCorrectas=cantidadPreguntasCorrectas;
+           _preguntas=preguntas;
+           _respuestas=respuestas;
+        }
+    public int UserName
+    {
+        get
+        {
+            return _username;
+        }
+        set{
+            _username = value;
+        }
+
+    }
+    public int PuntajeActual
+    {
+        get
+        {
+            return _puntajeActual;
+        }
+        set{
+            _puntajeActual = value;
+        }
+
+    }
+
+    public string CantidadPreguntasCorrectas
+    {
+        get
+        {
+            return _cantidadPreguntasCorrectas;
+        }
+        set{
+            _cantidadPreguntasCorrectas = value;
+        }
+    }
+
+     public List<Pregunta> Preguntas
+    {
+        get
+        {
+            return _preguntas;
+        }
+        set{
+            _preguntas = value;
+        }
+    }
+   public List<Pregunta> Respuestas
+    {
+        get
+        {
+            return _respuestas;
+        }
+        set{
+            _respuestas = value;
+        }
+    }
+}
+}
